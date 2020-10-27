ccfdbcb4cc3371a862f03c6464b0ead874a90dcb
Dylan Buchi <devybuchi@gmail.com>
2020-10-27 07:38:42 -0300

    add api function to get quote

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
+// Get Quote from Api
+async function getQuote() {
+    const proxy = "https://fast-eyrie-79953.herokuapp.com/";
+    const url =
+        "https://api.forismatic.com/api/1.0/method=getQuote&lang=en&format=json";
+    try {
+        const response = await fetch(proxy + url);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log("Error, No Quote"), error;
+    }
+}
 
+getQuote();
