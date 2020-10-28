f66bfeaee5b9e7ed820e5fcf47d8bcab79a9cdd6
Dylan Buchi <devybuchi@gmail.com>
2020-10-27 21:26:16 -0300

    add data from api to html page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,28 @@
+const quoteBox = document.getElementById("quote-box");
+const quoteAuthor = document.getElementById("author-name");
+const quoteText = document.getElementById("quote");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 // Get Quote from Api
 async function getQuote() {
     const proxy = "https://fast-eyrie-79953.herokuapp.com/";
     const url =
-        "https://api.forismatic.com/api/1.0/method=getQuote&lang=en&format=json";
+        "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
     try {
         const response = await fetch(proxy + url);
         const data = await response.json();
+
+        quoteAuthor.innerText = data.quoteAuthor;
+
+        if (!quoteAuthor.innerText) {
+            quoteAuthor.innerText = "Someone";
+        }
+
+        quoteText.innerText = data.quoteText;
         console.log(data);
     } catch (error) {
+        getQuote();
         console.log("Error, No Quote"), error;
     }
 }
